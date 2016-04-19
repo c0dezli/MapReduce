f0eed1c9c95861ab2f4c5421408a230c483b0105
steveleelx <steveleelx@gmail.com>
2016-04-19 17:09:07 -0400

    ????

--- mapreduce.c
+++ mapreduce.c
@@ -235,15 +235,15 @@ mr_destroy(struct map_reduce *mr) {
 int
 mr_finish(struct map_reduce *mr)
 {
- mr_printer(mr); 
- for(int i=0; i<(mr->n_threads); i++) {
+  mr_printer(mr);
+  for(int i=0; i<(mr->n_threads); i++) {
     if (pthread_join(mr->map_threads[i], NULL) != 0 || close(mr->infd[i]) == -1) {
       printf("map_thread %d exited\n", i);
       return -1;  // failed
     }
   }
   if(pthread_join(mr->reduce_thread, NULL) != 0 || close(mr->outfd) == -1){
-    printf("reduce_thread\n");
+    printf("reduce_thread\n", i);
     return -1;  // failed
   }
 
