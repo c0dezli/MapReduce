35d325418179a7c58fcf984116bac2e82bc71cae
steveleelx <steveleelx@gmail.com>
2016-04-19 17:40:47 -0400

    handle out of memory

--- mapreduce.c
+++ mapreduce.c
@@ -235,15 +235,12 @@ mr_destroy(struct map_reduce *mr) {
 int
 mr_finish(struct map_reduce *mr)
 {
-  for(int i=0; i<(mr->n_threads); i++) {
-    if(mr->map_threads != NULL && mr->infd != NULL) { // out of memory
-      if (pthread_join(mr->map_threads[i], NULL) != 0 || close(mr->infd[i]) == -1) {
-        printf("map_thread %d exited\n", i);
-        return -1;  // failed
-      }
+  if(mr = NULL && mr->map_threads != NULL && mr->infd != NULL) { // out of memory
+    for(int i=0; i<(mr->n_threads); i++) {
+        if (pthread_join(mr->map_threads[i], NULL) != 0 || close(mr->infd[i]) == -1) return -1;  // failed
     }
-  }
-  
+  } else return -1;
+
   if(pthread_join(mr->reduce_thread, NULL) != 0 || close(mr->outfd) == -1){
     printf("reduce_thread\n");
     return -1;  // failed
