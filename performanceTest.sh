27835950b739913084837c06fcb68328a6addd2b
jessetheman <jr2jr@ymail.com>
2016-04-21 18:31:04 -0400

    updated performanceTest

--- performanceTest.sh
+++ performanceTest.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-#arg1_=$2_
-#output_=$2
+thread_num=$1
+output_=$2
 total=0
 avg=0
 std=0
@@ -10,30 +10,30 @@ std=0
 
 for i in `seq 1 20`;  
 do
-#$1 arg1_ output_
+./mr-wordc thread_num output_
 #./testtest.c output_
 #array[i-1]=output_
-array[i-1]=6
+let "array[i-1]=output_"
 #total+=output_
-total+=6
+let "total+=output_"
 done
 
 
-avg=total/20
+let "avg=total/20"
 echo $avg
 
 for i in `seq 1 20`;
 do
-s1=array[i-1]-avg
-s2=s1*s1
-sarray[i-1]=s2
+let "s1=array[i-1]-avg"
+let "s2=s1*s1"
+let "sarray[i-1]=s2"
 done
 
 for i in `seq 1 20`;
 do
-total+=sarray[i-1]
+let "total+=sarray[i-1]"
 done
 
-std=total/20
+let "std=total/20"
 echo $std
 
