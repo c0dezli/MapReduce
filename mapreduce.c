0dc38994969e268725fc4e53c4ec335e907611ba
jessetheman <jr2jr@ymail.com>
2016-04-22 21:12:09 -0400

    change

--- mapreduce.c
+++ mapreduce.c
@@ -176,7 +176,7 @@ mr_destroy(struct map_reduce *mr) {
   free(mr->not_empty);
   free(mr->_lock);
 
-  free(mr->count);
+  //free(mr->count);
   free(mr->size);
   free(mr->args);
 
@@ -250,7 +250,8 @@ int
 mr_consume(struct map_reduce *mr, int id, struct kvpair *kv) {
   pthread_mutex_lock(&mr->_lock[id]); // lock
 
-  // make surewthere is value to consume
+ 
+ // make surewthere is value to consume
   while(mr->size[id] <= 0) {
     if(mr->mapfn_status[id] == 0){  // Map function done its work
       printf("DONE! Consume: ID = %d, mr->size[id] is %d, no more pairs, return 0\n", id, mr->size[id]);
